9fc2f303f01e4c335e27c4b985522b05bc05e83d
Garrett <ngarrett@iu.edu>
2020-08-12 16:55:50 -0400

    added analytics

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: process.env.ELEVENTY_ENV,
+  title: "Nate Garrett - User experience developer",
+  description: "",
+  baseurl: "https://nategarrett.com",
+  twitter: "@n8garrett"
+}
