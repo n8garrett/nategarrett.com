39edf11fa8f956bbca216cd81b309bc50c780a00
ngarrett <ngarrett@iupui.edu>
2021-01-15 14:45:39 -0500

    description update

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
-  title: "Nate Garrett - Web designer and developer",
+  title: "Nate Garrett - Web developer and digital marketer",
   description: "",
   baseurl: "https://nategarrett.com",
   twitter: "@n8garrett"
